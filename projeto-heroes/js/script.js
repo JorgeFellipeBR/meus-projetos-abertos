8af7bd0c780053e30654c8bb10c5a19edccc3d3c
JorgeFellipeBR <jorge10fellipe@gmail.com>
2024-05-22 14:12:27 -0300

    Atualizações no projeto

--- projeto-heroes/js/script.js
+++ projeto-heroes/js/script.js
@@ -2,7 +2,7 @@ function Ciclope() {
     let img = document.getElementById('imagemHero');
     let p = document.getElementById('detalhesDePersonagem');
 
-    img.src = '../img/x_men_97_cyclops_png_by_metropolis_hero1125_dh3rwot-414w-2x.png';
+    img.src = '../images/ciclope.png';
     img.style.width = '';
     p.innerHTML = 'Scott Summers, mais conhecido como Ciclope, é um dos membros mais icônicos dos X-Men. Desde sua primeira aparição em 1963,ele se tornou um símbolo da luta pela coexistência pacífica entre mutantes e humanos.';
 }
@@ -11,7 +11,7 @@ function Wolverine() {
     let img = document.getElementById('imagemHero');
     let p = document.getElementById('detalhesDePersonagem');
 
-    img.src = '../img/x_men_97_wolverine_png_by_metropolis_hero1125_dh3rwp9-414w-2x.png';
+    img.src = '../images/wolverine.png';
     img.style.width = '190px';
     p.innerHTML = 'Wolverine, também conhecido como Logan, é um personagem icônico dos X-Men da Marvel Comics. Ele é um mutante com diversas habilidades extraordinárias.';
 }
@@ -20,7 +20,7 @@ function Deadpool() {
     let img = document.getElementById('imagemHero');
     let p = document.getElementById('detalhesDePersonagem');
 
-    img.src = '../img/deadpool_render_1_by_goji1999_ddqsfb0-414w-2x.png';
+    img.src = '../images/deadpool.png';
     img.style.width = '';
     p.innerHTML = 'Deadpool, o anti-herói sarcástico da Marvel, é na verdade Wade Wilson, um ex-militar que se torna mercenário. Desesperado para curar seu câncer, ele participa de um experimento experimental que lhe concede um fator de cura regenerativo, mas o deixa terrivelmente desfigurado.';
 }
@@ -29,7 +29,7 @@ function Demolidor() {
     let img = document.getElementById('imagemHero');
     let p = document.getElementById('detalhesDePersonagem');
 
-    img.src = '../img/dfg5fqv-c2a6cdc6-0a79-4d1d-9ae5-7650ed0ce738.png';
+    img.src = '../images/demolidor.png';
     img.style.width = '';
     p.innerHTML = 'Matt Murdock, um advogado cego desde a infância, usa seus sentidos aguçados para combater o crime na cozinha do inferno, bairro de Nova York, como o vigilante Demolidor.';
 }
@@ -38,7 +38,7 @@ function Magneto() {
     let img = document.getElementById('imagemHero');
     let p = document.getElementById('detalhesDePersonagem');
 
-    img.src = '../img/Magneto-PNG-HD.png';
+    img.src = '../images/magneto.png';
     img.style.width = '';
     p.innerHTML = 'Magneto, é um mutante com o poder de controlar campos magnéticos. Ele é um defensor radical dos direitos mutantes, muitas vezes entrando em conflito com os X-Men e outros heróis. Apesar de seus métodos extremos, Magneto é uma figura complexa e motivada por um desejo genuíno de proteger seu povo da opressão.';
 }
@@ -47,7 +47,7 @@ function HomemDeFerro() {
     let img = document.getElementById('imagemHero');
     let p = document.getElementById('detalhesDePersonagem');
 
-    img.src = '../img/iron_man_aou_by_alexelz_d9595td-414w-2x.png';
+    img.src = '../images/homemdeferro.png';
     img.style.width = '';
     p.innerHTML = 'Tony Stark, um bilionário playboy e gênio inventor, é sequestrado por terroristas no Afeganistão. Ferido por um estilhaço de bomba em seu peito, ele constrói uma armadura poderosa para escapar e proteger seu coração. Nasce assim o Homem de Ferro, que usa sua tecnologia para combater o crime e defender o mundo.';
 }
