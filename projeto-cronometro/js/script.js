59be0edfcfe27e57101b71eef698d3abcc74dca5
JorgeFellipeBR <jorge10fellipe@gmail.com>
2024-05-23 18:07:44 -0300

    Novo projeto com atualizações futuras

---
+++ projeto-cronometro/js/script.js
@@ -0,0 +1,10 @@
+function Iniciar() {
+    let horas = document.getElementById('horas');
+    let minutos = document.getElementById('minutos');
+    let segundos = document.getElementById('segundos');
+    let milissegundos = document.getElementById('milissegundos');
+
+    setInterval(function () {
+        
+    }, 1000);
+}
