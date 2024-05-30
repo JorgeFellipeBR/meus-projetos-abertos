63ed6fba98c2f46b3e59e992fe127ced7d0a64e7
JorgeFellipeBR <jorge10fellipe@gmail.com>
2024-05-30 19:52:23 -0300

    Novo projeto com atualizações futuras

---
+++ projeto-site-generico/js/script.js
@@ -0,0 +1,9 @@
+// ABRIR SIDEBAR
+function abrirSidebar() {
+    document.getElementById('meuSidebar').style.width = '250px'
+}
+
+// FECHAR SIDEBAR
+function fecharSidebar() {
+    document.getElementById('meuSidebar').style.width = '0'
+}
