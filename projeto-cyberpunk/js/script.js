38d1193071fb1e40f9e4f3198017ee1fa1c0e12d
JorgeFellipeBR <jorge10fellipe@gmail.com>
2024-07-13 14:10:03 -0300

    Novo Projeto

---
+++ projeto-cyberpunk/js/script.js
@@ -0,0 +1,30 @@
+let fundo1 = document.getElementById('fundo1');
+let fundo2 = document.getElementById('fundo2');
+let fundo3 = document.getElementById('fundo3');
+let fundo4 = document.getElementById('fundo4');
+let fundo5 = document.getElementById('fundo5');
+let fundo6 = document.getElementById('fundo6');
+
+fundo1.addEventListener('click', function() {
+    document.body.style.backgroundImage = `url('fundo-1.jpg')`;
+});
+
+fundo2.addEventListener('click', function() {
+    document.body.style.backgroundImage = `url('fundo-2.jpg')`;
+});
+
+fundo3.addEventListener('click', function() {
+    document.body.style.backgroundImage = `url('fundo-3.jpg')`;
+});
+
+fundo4.addEventListener('click', function() {
+    document.body.style.backgroundImage = `url('fundo-4.jpg')`;
+});
+
+fundo5.addEventListener('click', function() {
+    document.body.style.backgroundImage = `url('fundo-5.jpg')`;
+});
+
+fundo6.addEventListener('click', function() {
+    document.body.style.backgroundImage = `url('fundo-6.jpg')`;
+});
