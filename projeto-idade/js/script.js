8000d6a59f2e9418854f74a2f8000e9f9fd82639
JorgeFellipeBR <jorge10fellipe@gmail.com>
2024-07-17 22:44:46 -0300

    Novo projeto

---
+++ projeto-idade/js/script.js
@@ -0,0 +1,12 @@
+let botao = document.getElementById('botao');
+let generoMasculino = document.getElementById('generoMasculino');
+let generoFeminino = document.getElementById('generoFeminino');
+let campoInput = document.getElementById('idade');
+let texto = document.getElementById('texto');
+let imagem = document.getElementById('imagemFoto');
+
+botao.addEventListener('click', function() {
+    let campoInputValor = campoInput.value;
+});
+
+
