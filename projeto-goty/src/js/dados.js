c72518b03417e6f806560749d58ed4c2f2d1aefc
JorgeFellipeBR <jorge10fellipe@gmail.com>
2024-09-09 17:04:48 -0300

    Novo projeto com atualizações futuras

---
+++ projeto-goty/src/js/dados.js
@@ -0,0 +1,86 @@
+let jogos = [
+    {
+      nome: "Madden NFL 2004 - Game do ano de 2003",
+      descricao: "Um jogo de futebol americano desenvolvido pela EA Sports, lançado em 2004."
+    },
+    {
+      nome: "GTA San Andreas - Game do ano de 2004",
+      descricao: "Um jogo de mundo aberto desenvolvido pela Rockstar Games, lançado em 2004."
+    },
+    {
+      nome: "Resident Evil 4 - Game do ano de 2005",
+      descricao: "Um jogo de terror e ação desenvolvido pela Capcom, lançado em 2005."
+    },
+    {
+      nome: "The Elder Scrolls IV: Oblivion - Game do ano de 2006",
+      descricao: "Um jogo de RPG de mundo aberto desenvolvido pela Bethesda Game Studios, lançado em 2006."
+    },
+    {
+      nome: "BioShock - Game do ano de 2007",
+      descricao: "Um jogo de tiro em primeira pessoa desenvolvido pela 2K Boston, lançado em 2007."
+    },
+    {
+      nome: "GTA IV - Game do ano de 2008",
+      descricao: "Um jogo de mundo aberto desenvolvido pela Rockstar North, lançado em 2008."
+    },
+    {
+      nome: "Uncharted 2 - Game do ano de 2009",
+      descricao: "Um jogo de ação e aventura desenvolvido pela Naughty Dog, lançado em 2009."
+    },
+    {
+      nome: "Red Dead Redemption - Game do ano de 2010",
+      descricao: "Um jogo de ação e aventura de mundo aberto desenvolvido pela Rockstar Games, lançado em 2010."
+    },
+    {
+      nome: "The Elder Scrolls V: Skyrim - Game do ano de 2011",
+      descricao: "Um jogo de RPG de mundo aberto desenvolvido pela Bethesda Game Studios, lançado em 2011."
+    },
+    {
+      nome: "The Walking Dead - Game do ano de 2012",
+      descricao: "Um jogo de aventura gráfica desenvolvido pela Telltale Games, lançado em 2012."
+    },
+    {
+      nome: "GTA V - Game do ano de 2013",
+      descricao: "Um jogo de mundo aberto desenvolvido pela Rockstar North, lançado em 2013."
+    },
+    {
+      nome: "Dragon Age: Inquisition - Game do ano de 2014",
+      descricao: "Um jogo de RPG de ação desenvolvido pela BioWare, lançado em 2014."
+    },
+    {
+      nome: "The Witcher 3: Wild Hunt - Game do ano de 2015",
+      descricao: "Um jogo de RPG de ação desenvolvido pela CD Projekt Red, lançado em 2015."
+    },
+    {
+      nome: "Overwatch - Game do ano de 2016",
+      descricao: "Um jogo de tiro em primeira pessoa desenvolvido pela Blizzard Entertainment, lançado em 2016."
+    },
+    {
+      nome: "The Legend of Zelda: Breath of the Wild - Game do ano de 2017",
+      descricao: "Um jogo de ação e aventura desenvolvido pela Nintendo, lançado em 2017."
+    },
+    {
+      nome: "God of War - Game do ano de 2018",
+      descricao: "Um jogo de ação e aventura desenvolvido pela Sony Santa Monica, lançado em 2018."
+    },
+    {
+      nome: "Sekiro: Shadows Die Twice - Game do ano de 2019",
+      descricao: "Um jogo de ação e aventura desenvolvido pela FromSoftware, lançado em 2019."
+    },
+    {
+      nome: "The Last of Us Part 2 - Game do ano de 2020",
+      descricao: "Um jogo de ação e aventura desenvolvido pela Naughty Dog, lançado em 2020."
+    },
+    {
+      nome: "It Takes Two - Game do ano de 2021",
+      descricao: "Um jogo de plataforma cooperativo desenvolvido pela Hazelight Studios, lançado em 2021."
+    },
+    {
+      nome: "Elden Ring - Game do ano de 2022",
+      descricao: "Um jogo de RPG de ação desenvolvido pela FromSoftware, lançado em 2022."
+    },
+    {
+      nome: "Baldur's Gate 3 - Game do ano de 2023",
+      descricao: "Um jogo de RPG de computador desenvolvido pela Larian Studios, lançado em 2023."
+    }
+];
