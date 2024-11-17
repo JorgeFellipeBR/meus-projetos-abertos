a722ac2624fc6f899de5a8f8773ee78402d2469b
JorgeFellipeBR <jorge10fellipe@gmail.com>
2024-11-17 15:05:18 -0300

    Update na pasta meus projetos

--- projeto-site-generico/js/script.js
+++
@@ -1,9 +0,0 @@
-// ABRIR SIDEBAR
-function abrirSidebar() {
-    document.getElementById('meuSidebar').style.width = '250px'
-}
-
-// FECHAR SIDEBAR
-function fecharSidebar() {
-    document.getElementById('meuSidebar').style.width = '0'
-}
