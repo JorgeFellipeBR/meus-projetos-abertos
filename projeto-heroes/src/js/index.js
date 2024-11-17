afa36b99036e8dedb15791104b1e886e3807c958
JorgeFellipeBR <jorge10fellipe@gmail.com>
2024-11-16 22:07:25 -0300

    Atualização de responsividade no projeto

--- projeto-heroes/src/js/index.js
+++ projeto-heroes/src/js/index.js
@@ -1,53 +1,36 @@
-function Ciclope() {
-    let img = document.getElementById('imagemHero');
-    let p = document.getElementById('detalhesDePersonagem');
+let listagemHeroi = document.querySelectorAll(".heroi");
 
-    img.src = '../images/ciclope.png';
-    img.style.width = '';
-    p.innerHTML = 'Scott Summers, mais conhecido como Ciclope, é um dos membros mais icônicos dos X-Men. Desde sua primeira aparição em 1963,ele se tornou um símbolo da luta pela coexistência pacífica entre mutantes e humanos.';
+listagemHeroi.forEach(heroi => {
+    heroi.addEventListener("click", () => {
+        esconderCartaoHeroi();
+
+        return mostrarCartaoHeroiSelecionado(heroi);
+
+        desativarHeroiNaListagem();
+
+        ativarHeroiSelecionadoNaListagem();
+    })
+})
+
+function ativarHeroiSelecionadoNaListagem() {
+    const heroiSelecionadoNaListagem = document.getElementById("idHeroiSelecionado");
+    heroiSelecionadoNaListagem.classList.add("ativo");
 }
 
-function Wolverine() {
-    let img = document.getElementById('imagemHero');
-    let p = document.getElementById('detalhesDePersonagem');
-
-    img.src = '../images/wolverine.png';
-    img.style.width = '190px';
-    p.innerHTML = 'Wolverine, também conhecido como Logan, é um personagem icônico dos X-Men da Marvel Comics. Ele é um mutante com diversas habilidades extraordinárias.';
+function desativarHeroiNaListagem() {
+    const heroiAtivoNaListagem = document.querySelector(".ativo");
+    heroiAtivoNaListagem.classList.remove("ativo");
 }
 
-function Deadpool() {
-    let img = document.getElementById('imagemHero');
-    let p = document.getElementById('detalhesDePersonagem');
-
-    img.src = '../images/deadpool.png';
-    img.style.width = '';
-    p.innerHTML = 'Deadpool, o anti-herói sarcástico da Marvel, é na verdade Wade Wilson, um ex-militar que se torna mercenário. Desesperado para curar seu câncer, ele participa de um experimento experimental que lhe concede um fator de cura regenerativo, mas o deixa terrivelmente desfigurado.';
+function mostrarCartaoHeroiSelecionado(heroi) {
+    const idHeroiSelecionado = heroi.attributes.id.value;
+    const idDoCartaoHeroiParaAbrir = "cartao-" + idHeroiSelecionado;
+    const cartaoHeroiParaAbrir = document.getElementById(idDoCartaoHeroiParaAbrir);
+    cartaoHeroiParaAbrir.classList.add("aberto");
+    return idHeroiSelecionado;
 }
 
-function Demolidor() {
-    let img = document.getElementById('imagemHero');
-    let p = document.getElementById('detalhesDePersonagem');
-
-    img.src = '../images/demolidor.png';
-    img.style.width = '';
-    p.innerHTML = 'Matt Murdock, um advogado cego desde a infância, usa seus sentidos aguçados para combater o crime na cozinha do inferno, bairro de Nova York, como o vigilante Demolidor.';
-}
-
-function Magneto() {
-    let img = document.getElementById('imagemHero');
-    let p = document.getElementById('detalhesDePersonagem');
-
-    img.src = '../images/magneto.png';
-    img.style.width = '';
-    p.innerHTML = 'Magneto, é um mutante com o poder de controlar campos magnéticos. Ele é um defensor radical dos direitos mutantes, muitas vezes entrando em conflito com os X-Men e outros heróis. Apesar de seus métodos extremos, Magneto é uma figura complexa e motivada por um desejo genuíno de proteger seu povo da opressão.';
-}
-
-function HomemDeFerro() {
-    let img = document.getElementById('imagemHero');
-    let p = document.getElementById('detalhesDePersonagem');
-
-    img.src = '../images/homemdeferro.png';
-    img.style.width = '';
-    p.innerHTML = 'Tony Stark, um bilionário playboy e gênio inventor, é sequestrado por terroristas no Afeganistão. Ferido por um estilhaço de bomba em seu peito, ele constrói uma armadura poderosa para escapar e proteger seu coração. Nasce assim o Homem de Ferro, que usa sua tecnologia para combater o crime e defender o mundo.';
+function esconderCartaoHeroi() {
+    const cartaoHeroiAberto = document.querySelector(".aberto");
+    cartaoHeroiAberto.classList.remove("aberto");
 }
