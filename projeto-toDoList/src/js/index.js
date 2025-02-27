d946a4fe761d01817ef1e827b79d6e553e23d6e8
JorgeFellipeBR <jorge10fellipe@gmail.com>
2025-02-27 14:34:18 -0300

    Atualização com novas mecânicas no projeto to-do list

--- projeto-toDoList/src/js/index.js
+++ projeto-toDoList/src/js/index.js
@@ -1,9 +1,13 @@
 let botao = document.getElementById("botao");
+let botaoNovo = document.getElementById("botaoNovo");
 let campoTexto = document.getElementById("campoTexto");
+let novoTexto = document.getElementById("novoTexto");
 let tarefas = document.getElementById("tarefas");
 let tarefaTexto = document.getElementById("tarefaTexto");
-let editar = document.getElementById("editat");
 let remover = document.getElementById("remover");
+let eliminar = document.getElementById("eliminar");
+let container = document.getElementById("container");
+
 let contador = 0;
 
 botao.addEventListener("click", () => {
@@ -19,7 +23,7 @@ botao.addEventListener("click", () => {
                 <div class="radio">
                     <input type="checkbox" name="trf" onclick="marcaTarefa(${contador})">
                 </div>
-                <span class="tarefaTexto" id="tarefaTexto">${campoTextoValor}</span>
+                <span class="tarefaTexto" id="campo-${contador}">${campoTextoValor}</span>
                 <button class="editar" id="editar">
                     <span class="material-symbols-outlined">
                         edit_note
@@ -34,8 +38,31 @@ botao.addEventListener("click", () => {
         `;
 
         tarefas.innerHTML += novoItem;
+
+        let idCampo = "campo-" + contador;
+        let n = document.getElementById(idCampo);
+        botaoNovo.addEventListener("click", () => {
+            let novoTextoValor = novoTexto.value;
+            n.innerHTML = novoTextoValor;
+            container.classList.remove("aparecer");
+            novoTexto.value = "";
+        })
     }
+
+    let x = "editar"
+    let editar = document.querySelectorAll("#"+x);
+
+    editar.forEach((a) => {
+        a.addEventListener("click", () => {
+            container.classList.add("aparecer");
+        })
+    })
 })
+
+eliminar.addEventListener("click", () => {
+    container.classList.remove("aparecer");
+})
+
 
 function eliminarElemento(removerElemento) {
     let remove = document.getElementById(removerElemento);
